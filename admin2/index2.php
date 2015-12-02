8f0d22260d86bc24afe8196f40b81bf9bd9545cd
unknown <jdekhtia@GSMPORT11.utc>
2015-12-02 09:27:26 +0100

    Admin2 menu fixed

--- admin2/index2.php
+++ admin2/index2.php
@@ -1,13 +1,4 @@
-<?php
-	// Page d'accueil : /index.php
-	header("Content-Type: text/html; charset=UTF-8");
-	$root = realpath($_SERVER["DOCUMENT_ROOT"]);
-	require_once $root.'/config.inc.php';
-	require_once $root.'/inc/checkadmin.php';
-	
-?>
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta content="IE=edge" http-equiv="X-UA-Compatible">
@@ -16,7 +7,7 @@
 	<meta content="Neon Admin Panel" name="description">
 	<meta content="Laborator.co" name="author">
 	<link href="assets/images/favicon.ico" rel="icon">
-	<title><?php echo $_CONFIG["website"]["title"]; ?></title>
+	<title>Neon</title>
 	<link href="assets/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css"
 	id="style-resource-1" rel="stylesheet">
 	<link href="assets/css/font-icons/entypo/css/entypo.css" id="style-resource-2"
@@ -42,13 +33,217 @@
 <body class="page-body page-fade" data-url="localhost">
 	<div class="page-container">
 		<?php
-			include("parts/menu.php");
+			include("parts/menu2.php");
 		?>
 
 
 
 		<div class="main-content">
-
+			<!-- TS14489286585191: Xenon - Boostrap Admin Template created by Laborator / Please buy this theme and support the updates -->
+			<div class="row">
+				<!-- Profile Info and Notifications -->
+				<div class="col-md-6 col-sm-8 clearfix">
+					<ul class="user-info pull-left pull-none-xsm">
+						<!-- Profile Info -->
+						<li class="profile-info dropdown">
+							<!-- add class "pull-right" if you want to place this from right -->
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#"><img alt=""
+							class="img-circle" src="assets/images/thumb-1%402x.png" width="44"> John Henderson</a>
+							<ul class="dropdown-menu">
+								<!-- Reverse Caret -->
+								<li class="caret"></li><!-- Profile sub-links -->
+								<li>
+									<a href="extra/timeline/index.html"><i class="entypo-user"></i> Edit
+									Profile</a>
+								</li>
+								<li>
+									<a href="mailbox/main/index.html"><i class="entypo-mail"></i>
+									Inbox</a>
+								</li>
+								<li>
+									<a href="extra/calendar/index.html"><i class="entypo-calendar"></i>
+									Calendar</a>
+								</li>
+								<li>
+									<a href="#"><i class="entypo-clipboard"></i> Tasks</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<ul class="user-info pull-left pull-right-xs pull-none-xsm">
+						<!-- Raw Notifications -->
+						<li class="notifications dropdown">
+							<a class="dropdown-toggle" data-close-others="true" data-hover=
+							"dropdown" data-toggle="dropdown" href="#"><i class=
+							"entypo-attention"></i> <span class="badge badge-info">6</span></a>
+							<ul class="dropdown-menu">
+								<!-- TS144892865817701: Xenon - Boostrap Admin Template created by Laborator / Please buy this theme and support the updates -->
+								<li class="top">
+									<p class="small"><a class="pull-right" href="#">Mark all Read</a> You
+									have <strong>3</strong> new notifications.</p>
+								</li>
+								<li>
+									<ul class="dropdown-menu-list scroller">
+										<li class="unread notification-success">
+											<a href="#"><i class="entypo-user-add pull-right"></i> <span class=
+											"line"><strong>New user registered</strong></span> <span class=
+											"line small">30 seconds ago</span></a>
+										</li>
+										<li class="unread notification-secondary">
+											<a href="#"><i class="entypo-heart pull-right"></i> <span class=
+											"line"><strong>Someone special liked this</strong></span>
+											<span class="line small">2 minutes ago</span></a>
+										</li>
+										<li class="notification-primary">
+											<a href="#"><i class="entypo-user pull-right"></i> <span class=
+											"line"><strong>Privacy settings have been changed</strong></span>
+											<span class="line small">3 hours ago</span></a>
+										</li>
+										<li class="notification-danger">
+											<a href="#"><i class="entypo-cancel-circled pull-right"></i>
+											<span class="line">John cancelled the event</span> <span class=
+											"line small">9 hours ago</span></a>
+										</li>
+										<li class="notification-info">
+											<a href="#"><i class="entypo-info pull-right"></i> <span class=
+											"line">The server is status is stable</span> <span class=
+											"line small">yesterday at 10:30am</span></a>
+										</li>
+										<li class="notification-warning">
+											<a href="#"><i class="entypo-rss pull-right"></i> <span class=
+											"line">New comments waiting approval</span> <span class=
+											"line small">last week</span></a>
+										</li>
+									</ul>
+								</li>
+								<li class="external">
+									<a href="#">View all notifications</a>
+								</li>
+							</ul>
+						</li><!-- Message Notifications -->
+						<li class="notifications dropdown">
+							<a class="dropdown-toggle" data-close-others="true" data-hover=
+							"dropdown" data-toggle="dropdown" href="#"><i class="entypo-mail"></i>
+							<span class="badge badge-secondary">10</span></a>
+							<ul class="dropdown-menu">
+								<!-- TS14489286587733: Xenon - Boostrap Admin Template created by Laborator / Please buy this theme and support the updates -->
+								<li>
+									<form class="top-dropdown-search">
+										<div class="form-group">
+											<input class="form-control" name="s" placeholder=
+											"Search anything..." type="text">
+										</div>
+									</form>
+									<ul class="dropdown-menu-list scroller">
+										<li class="active">
+											<a href="#"><span class="image pull-right"><img alt="" class=
+											"img-circle" src="assets/images/thumb-1%402x.png" width="44"></span>
+											<span class="line"><strong>Luc Chartier</strong> - yesterday</span>
+											<span class="line desc small">This ain�t our first item, it is the
+											best of the rest.</span></a>
+										</li>
+										<li class="active">
+											<a href="#"><span class="image pull-right"><img alt="" class=
+											"img-circle" src="assets/images/thumb-2%402x.png" width="44"></span>
+											<span class="line"><strong>Salma Nyberg</strong> - 2 days ago</span>
+											<span class="line desc small">Oh he decisively impression attachment
+											friendship so if everything.</span></a>
+										</li>
+										<li>
+											<a href="#"><span class="image pull-right"><img alt="" class=
+											"img-circle" src="assets/images/thumb-3%402x.png" width="44"></span>
+											<span class="line">Hayden Cartwright - a week ago</span>
+											<span class="line desc small">Whose her enjoy chief new young.
+											Felicity if ye required likewise so doubtful.</span></a>
+										</li>
+										<li>
+											<a href="#"><span class="image pull-right"><img alt="" class=
+											"img-circle" src="assets/images/thumb-4%402x.png" width="44"></span>
+											<span class="line">Sandra Eberhardt - 16 days ago</span>
+											<span class="line desc small">On so attention necessary at by
+											provision otherwise existence direction.</span></a>
+										</li>
+									</ul>
+								</li>
+								<li class="external">
+									<a href="mailbox/main/index.html">All Messages</a>
+								</li>
+							</ul>
+						</li><!-- Task Notifications -->
+						<li class="notifications dropdown">
+							<a class="dropdown-toggle" data-close-others="true" data-hover=
+							"dropdown" data-toggle="dropdown" href="#"><i class="entypo-list"></i>
+							<span class="badge badge-warning">1</span></a>
+							<ul class="dropdown-menu">
+								<!-- TS144892865814224: Xenon - Boostrap Admin Template created by Laborator / Please buy this theme and support the updates -->
+								<li class="top">
+									<p>You have 6 pending tasks</p>
+								</li>
+								<li>
+									<ul class="dropdown-menu-list scroller">
+										<li>
+											<a href="#"><span class="task"><span class="desc">Procurement</span>
+											<span class="percent">27%</span></span> <span class=
+											"progress"><span class="progress-bar progress-bar-success" style=
+											"width: 27%;"><span class="sr-only">27%
+											Complete</span></span></span></a>
+										</li>
+										<li>
+											<a href="#"><span class="task"><span class="desc">App
+											Development</span> <span class="percent">83%</span></span>
+											<span class="progress progress-striped"><span class=
+											"progress-bar progress-bar-danger" style="width: 83%;"><span class=
+											"sr-only">83% Complete</span></span></span></a>
+										</li>
+										<li>
+											<a href="#"><span class="task"><span class="desc">HTML
+											Slicing</span> <span class="percent">91%</span></span> <span class=
+											"progress"><span class="progress-bar progress-bar-success" style=
+											"width: 91%;"><span class="sr-only">91%
+											Complete</span></span></span></a>
+										</li>
+										<li>
+											<a href="#"><span class="task"><span class="desc">Database
+											Repair</span> <span class="percent">12%</span></span> <span class=
+											"progress progress-striped"><span class=
+											"progress-bar progress-bar-warning" style="width: 12%;"><span class=
+											"sr-only">12% Complete</span></span></span></a>
+										</li>
+										<li>
+											<a href="#"><span class="task"><span class="desc">Backup Create
+											Progress</span> <span class="percent">54%</span></span> <span class=
+											"progress progress-striped"><span class=
+											"progress-bar progress-bar-info" style="width: 54%;"><span class=
+											"sr-only">54% Complete</span></span></span></a>
+										</li>
+										<li>
+											<a href="#"><span class="task"><span class="desc">Upgrade
+											Progress</span> <span class="percent">17%</span></span> <span class=
+											"progress progress-striped"><span class=
+											"progress-bar progress-bar-important" style=
+											"width: 17%;"><span class="sr-only">17%
+											Complete</span></span></span></a>
+										</li>
+									</ul>
+								</li>
+								<li class="external">
+									<a href="#">See all tasks</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div><!-- Raw Links -->
+				<div class="col-md-6 col-sm-4 clearfix hidden-xs">
+					<ul class="list-inline links-list pull-right">
+						<li>
+							<a href="extra/login/index.html">Log Out <i class=
+							"entypo-logout right"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<hr>
+			<!-- TS144892865817894: Xenon - Boostrap Admin Template created by Laborator / Please buy this theme and support the updates -->
 			<script type="text/javascript">
 			jQuery(document).ready(function($)
 			{
